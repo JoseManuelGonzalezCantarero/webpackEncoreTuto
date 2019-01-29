70dad9db452c59cc9059f5d6bbb394e211719d5b
Manu <jgonzalezcantarero@gmail.com>
2019-01-29 15:38:54 +0100

    Multiple pages / entries episode ( chapter 4 )

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ Encore
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
     .addEntry('rep_log', './public/assets/js/rep_log.js')
+    .addEntry('login', './public/assets/js/login.js')
+    .addEntry('layout', './public/assets/js/layout.js')
     .enableBuildNotifications()
 ;
 // export the final configuration
