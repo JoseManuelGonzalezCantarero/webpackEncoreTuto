16e396a9c8407a712c0bc9bb07942e7ecbc8f58c
Manu <jgonzalezcantarero@gmail.com>
2019-01-29 15:59:47 +0100

    Require CSS!? episode ( chapter 4 )

--- public/assets/js/layout.js
+++ public/assets/js/layout.js
@@ -3,6 +3,9 @@
 const $ = require('jquery');
 global.$ = $;
 require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
+require('../css/main.css');
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
