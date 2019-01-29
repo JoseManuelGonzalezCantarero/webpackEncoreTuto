e9c08cb9e02693794766e409bc9165190dce3e79
Manu <jgonzalezcantarero@gmail.com>
2019-01-29 17:18:55 +0100

    Building for production episode ( chapter 4 )

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ Encore
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
+    .createSharedEntry('layout', './assets/js/layout.js')
     .addEntry('rep_log', './assets/js/rep_log.js')
     .addEntry('login', './assets/js/login.js')
-    .addEntry('layout', './assets/js/layout.js')
     .enableBuildNotifications()
     .autoProvidejQuery()
     .copyFiles({
