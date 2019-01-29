1aad91b76044304d31532f26011d316bfeb475f9
Manu <jgonzalezcantarero@gmail.com>
2019-01-29 17:11:38 +0100

    ES6 import & export episode ( chapter 4 )

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const $ = require('jquery');
+import $ from 'jquery';
 global.$ = $;
-require('bootstrap');
-require('bootstrap/dist/css/bootstrap.css');
-require('font-awesome/css/font-awesome.css');
-require('../css/main.scss');
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import '../css/main.scss';
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
