006c406012ad68f705dbd270a2951471d234d8b4
Manu <jgonzalezcantarero@gmail.com>
2019-01-29 16:21:28 +0100

    Handling images with CopyFile episode ( chapter 4 )

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,15 @@ Encore
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
-    .addEntry('rep_log', './public/assets/js/rep_log.js')
-    .addEntry('login', './public/assets/js/login.js')
-    .addEntry('layout', './public/assets/js/layout.js')
+    .addEntry('rep_log', './assets/js/rep_log.js')
+    .addEntry('login', './assets/js/login.js')
+    .addEntry('layout', './assets/js/layout.js')
     .enableBuildNotifications()
     .autoProvidejQuery()
+    .copyFiles({
+        from: './assets/static',
+        to: 'static/[path][name].[ext]'
+    })
 ;
 // export the final configuration
 module.exports = Encore.getWebpackConfig();
