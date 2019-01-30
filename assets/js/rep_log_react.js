156a0db8573e630e7c294caa0dd4e6e00930ec8a
Manu <jgonzalezcantarero@gmail.com>
2019-01-30 11:26:49 +0100

    React.createElement() episode ( chapter 5 )

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,10 @@
-console.log('Oh hallo React peeps! 🏋️');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el = React.createElement(
+    'h2',
+    null,
+    'Lift History!',
+    React.createElement('span', null, '❤️')
+);
+ReactDom.render(el, document.getElementById('lift-stuff-app'));
