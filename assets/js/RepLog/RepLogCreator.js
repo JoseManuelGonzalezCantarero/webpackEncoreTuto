192b58f7205616307fefd3ea328099b3e79b256b
Manu <jgonzalezcantarero@gmail.com>
2019-02-06 11:54:27 +0100

    Immutability / Don't mutate my state episode ( chapter 5 )

--- assets/js/RepLog/RepLogCreator.js
+++ assets/js/RepLog/RepLogCreator.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import uuid from "uuid/v4";
 
 export default class RepLogCreator extends Component {
     constructor(props) {
@@ -8,13 +9,19 @@ export default class RepLogCreator extends Component {
         this.quantityInput = React.createRef();
         this.itemSelect = React.createRef();
 
+        this.itemOptions = [
+            { id: 'cat', text: 'Cat' },
+            { id: 'fat_cat', text: 'Big Fat Cat' },
+            { id: 'laptop', text: 'My Laptop' },
+            { id: 'coffee_cup', text: 'Coffee Cup' },
+        ];
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
     handleFormSubmit(event) {
         event.preventDefault();
 
-        const { onNewItemSubmit } = this.props;
+        const { onAddRepLog } = this.props;
         const quantityInput = this.quantityInput.current;
         const itemSelect = this.itemSelect.current;
 
@@ -22,7 +29,7 @@ export default class RepLogCreator extends Component {
         console.log(quantityInput.value);
         console.log(itemSelect.options[itemSelect.selectedIndex].value);
 
-        onNewItemSubmit(
+        onAddRepLog(
             itemSelect.options[itemSelect.selectedIndex].text,
             quantityInput.value
         );
@@ -42,11 +49,9 @@ export default class RepLogCreator extends Component {
                             ref={this.itemSelect}
                             required="required"
                             className="form-control">
-                        <option value="" defaultValue="">What did you lift?</option>
-                        <option value="cat">Cat</option>
-                        <option value="fat_cat">Big Fat Cat</option>
-                        <option value="laptop">My Laptop</option>
-                        <option value="coffee_cup">Coffee Cup</option>
+                            {this.itemOptions.map(option => {
+                                return <option value={option.id} key={option.id}>{option.text}</option>
+                            })}
                     </select>
                 </div>
                 {' '}
@@ -67,5 +72,5 @@ export default class RepLogCreator extends Component {
 }
 
 RepLogCreator.propTypes = {
-    onNewItemSubmit: PropTypes.func.isRequired
+    onAddRepLog: PropTypes.func.isRequired
 };
