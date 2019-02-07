c57b3b53f0ec9c600f8f462d07594d271a83db1e
Manu <jgonzalezcantarero@gmail.com>
2019-02-07 16:19:46 +0100

    Hitting the delete endpoint episode ( chapter 5 )

--- assets/js/api/rep_log_api.js
+++ assets/js/api/rep_log_api.js
@@ -1,9 +1,19 @@
+function fetchJson(url, options) {
+    return fetch(url, Object.assign({
+        credentials: 'same-origin',
+    }, options))
+    .then(response => {
+        return response.json();
+    });
+}
+
 export function getRepLogs() {
-    return fetch('/reps', {
-        credentials: 'same-origin'
-    })
-        .then(response => {
-            return response.json()
-                .then((data) => data.items);
-        });
+    return fetchJson('/reps')
+        .then(data => data.items);
+}
+
+export function deleteRepLog(id) {
+    return fetchJson(`/reps/${id}`, {
+        method: 'DELETE'
+    });
 }
